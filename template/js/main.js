6539be9a2de3334b2d91fea3c836db85407e6376
Solomon <solnsumei@gmail.com>
2017-07-30 14:15:19 +0100

    Added html template files with css and js files for the hello -books application

---
+++ template/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(() => {
+    /*let formShown = "login";
+
+    showForm();
+
+    $('#show-registration-form').on('click', ()=>{
+        formShown = "register";
+        showForm();
+    })
+
+    $('#show-login-form').on('click', ()=>{
+        formShown = "login";
+        showForm();
+    });
+
+    function showForm(){
+        if(formShown == "login"){
+            $('#login-form').show();
+            $('#registration-form').hide();
+        }else{
+            $('#login-form').hide();
+            $('#registration-form').removeClass('hidden');
+            $('#registration-form').show();
+        }
+    }*/
+});
