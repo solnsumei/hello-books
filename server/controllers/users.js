cfdfdca5089f61a2b98737eb14f323156ac51fba
Solomon <solnsumei@gmail.com>
2017-08-05 14:30:05 +0100

    Add validation to user login method

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -42,6 +42,18 @@ export const usersController = {
 
     // Authenticate users
     login(req, res){
+        if(req.body.constructor === Object && Object.keys(req.body).length === 0){
+            return res.status(400).send({error : 'All fields are required!'});
+        }
+
+        req.sanitizeBody('username').trim();
+        req.checkBody('username', 'Username is required').notEmpty();
+
+        req.getValidationResult().then((result) => {
+            if(!result.isEmpty()){
+                return res.status(400).send(result.array());
+            }
+        });
 
     },
 };
