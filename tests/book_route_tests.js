9f3808121ecf0b34f311914c25c6a65d585f7b83
Solomon <solnsumei@gmail.com>
2017-08-05 21:08:39 +0100

    Test middleware routes

--- tests/book_route_tests.js
+++ tests/book_route_tests.js
@@ -8,12 +8,14 @@ describe('Book Routes', () => {
 
     describe('GET /api/books', () => {
 
-        it('it should respond with a 200 status code', (done) => {
-            request(app)
-                .get('/api/books')
-                .set('Accept', 'application/json')
-                .expect(200)
-                .expect('Content-Type', /json/, done)
+        describe('GET books without being logged in', () => {
+            it('it should respond with a 403 status code', (done) => {
+                request(app)
+                    .get('/api/books')
+                    .set('Accept', 'application/json')
+                    .expect(403)
+                    .expect('Content-Type', /json/, done)
+            });
         });
 
     });
@@ -25,35 +27,35 @@ describe('Book Routes', () => {
             done();
         });
 
-        it('responds with a 404 not found', (done) => {
-            request(app)
-                .post('/api/book')
-                .send({ title: 'My Book', author: 'Andela', description:'', quantity:'-1', pic:null })
-                .set('Accept', 'application/json')
-                .expect(404)
-                .expect('Content-Type',  /html/, done)
+        describe('POST without admin privileges', ()=> {
+            it('responds with a 403 Access denied', (done) => {
+                request(app)
+                    .post('/api/book')
+                    .send({ title: 'My Book', author: 'Andela', description:'', quantity:'-1', pic:null })
+                    .set('Accept', 'application/json')
+                    .expect(403)
+                    .expect('Content-Type',  /json/, done)
+            });
+
+            it('responds with a 403 Access denied', (done) => {
+                request(app)
+                    .post('/api/books')
+                    .send({ title: 'My Book', author: 'Andela', description:'', quantity:'-1', pic:null })
+                    .set('Accept', 'application/json')
+                    .expect(403)
+                    .expect('Content-Type', /json/, done)
+            });
+
+            it('responds with a 403 Access denied', (done) => {
+                request(app)
+                    .post('/api/books')
+                    .send({ title: 'My Book', author: 'Andela', description: 'This is the first book in the library', quantity:4, pic:'mybook.jpg' })
+                    .set('Accept', 'application/json')
+                    .expect(403)
+                    .expect('Content-Type', /json/, done);
+            });
         });
 
-        it('responds with a 400 bad request', (done) => {
-            request(app)
-                .post('/api/books')
-                .send({ title: 'My Book', author: 'Andela', description:'', quantity:'-1', pic:null })
-                .set('Accept', 'application/json')
-                .expect(400)
-                .expect('Content-Type', /json/, done)
-        });
-
-        it('responds with a 201 with created book model', (done) => {
-            request(app)
-                .post('/api/books')
-                .send({ title: 'My Book', author: 'Andela', description: 'This is the first book in the library', quantity:4, pic:'mybook.jpg' })
-                .set('Accept', 'application/json')
-                .expect(201)
-                .expect('Content-Type', /json/)
-                .expect(/"title":\s*"My Book"/)
-                .expect(/"author":\s*"Andela"/)
-                .expect(/"quantity":\s*4/, done);
-        });
     });
 
 });
