3c4d26014a20669f11ddb7a5028af5a86757cc21
Solomon <solnsumei@gmail.com>
2017-08-06 14:32:22 +0100

    Finish update method in booksController
    Write validation rules for query parameter and sanitize the bookId parameter. Test using postman for functionality.

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -39,7 +39,44 @@ export const booksController = {
 
     // Method to update a book
     update(req, res){
-        
-    }
+        if(req.body.constructor === Object && Object.keys(req.body).length === 0){
+            return res.status(400).send({error : 'All fields are required!'});
+        }
+
+        if(req.params.bookId === null){
+            return res.status(400).send({error : 'Book id cannot be null'});
+        }
+
+        req.checkParams('bookId', 'BookId is invalid').isNumeric();
+
+        req.getValidationResult()
+            .then(result => {
+               if(!result.isEmpty()){
+                   return res.status(400).send(result.array());
+               }
+        });
+
+        return db.Book
+            .findById(req.params.bookId)
+            .then(book => {
+                if(!book){
+                    return res.status(404).send({error: `Book not found`});
+                }
+                book.update({
+                    title: req.body.title,
+                    author: req.body.author,
+                    description: req.body.description,
+                    pic: req.body.pic
+                }).then(result => res.status(200).send(result))
+                    .catch(error => {
+                        if(error.name == "SequelizeUniqueConstraintError" || error.name == "SequelizeValidationError"){
+                            res.status(400).send(error.errors);
+                        }else{
+                            res.status(400).send(error);
+                        }
+                    });
+
+            });
+    },
 
 };
