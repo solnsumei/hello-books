3138b19a7cd74485e7b7f620aff8779f51e20b02
Solomon <solnsumei@gmail.com>
2017-09-06 04:56:14 +0100

    Add migration and seeder files

---
+++ server/migrations/20170821125606-create-membership-type.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('MembershipTypes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      membershipType: {
+        allowNull: false,
+        unique:true,
+        type: Sequelize.STRING
+      },
+      lendDuration: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      maxBorrowable: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('MembershipTypes');
+  }
+};
