2fdf1277f0ca43136112aea9f40da6019d03c01e
solnsumei <solnsumei@gmail.com>
2017-09-12 06:21:47 +0100

    Modify Book detail component to use book component

--- client/src/app/components/Book.js
+++ client/src/app/components/Book.js
@@ -10,6 +10,41 @@ export default class Book extends React.Component {
  * @return {[type]} [description]
  */
   render() {
+    if (!this.props.link) {
+      return (
+        <div className="row">
+          <div className="col s12 m5">
+            <h3>
+              <strong>{this.props.title}</strong>
+            </h3>
+            <p className="offset-3"><i>By {this.props.author}</i></p>
+
+            {this.props.description}
+
+          </div>
+
+          <div className="col s12 m4">
+            <div className="card">
+              <div className="card-image waves-effect waves-block waves-light">
+                <img src={this.props.coverPic} />
+              </div>
+            </div>
+          </div>
+
+          <div className="col s12 m3">
+            <p>({this.props.category})</p>
+            <p>{this.props.numberOfPages} pages</p>
+            <p>Status: <label className="label label-success">{this.props.status}</label> </p>
+            <button className="btn teal" id="borrow-book">
+              Borrow
+            </button>
+            <p><Link to="/catalog">Back to Catalog</Link></p>
+          </div>
+        </div>
+
+      );
+    }
+
     return (
       <div className="col s12 m6">
         <div className="row">
