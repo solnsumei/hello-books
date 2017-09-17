4a2e262afe5aa37e1d180172fe7a32138bb35220
solnsumei <solnsumei@gmail.com>
2017-09-17 23:17:14 +0100

    Fix bug in profile page

--- client/src/app/components/users/ProfilePage.js
+++ client/src/app/components/users/ProfilePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import UserDetail from './UserDetail';
 import BorrowedItem from './BorrowedItem';
+import TopTitle from '../common/TopTitle';
 
 /**
  *
